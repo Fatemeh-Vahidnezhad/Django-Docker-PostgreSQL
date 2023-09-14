1fd04ae84dac48ab12f3acaa33566d53b756d816
Fatemeh-Vahidnezhad <Fatemeh.Vahidnezhad@gmail.com>
2023-09-14 02:56:04 -0700

    add environmental variables for Secret Key & Debug in settings

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jl&h!vp_=v*vt87&()=9q8q_2-udxncdxma^fd8+@7kws!8f^4'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env.bool("DJANGO_DEBUG")
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'localhost', '.herokuapp.com']
 
 
 # Application definition
